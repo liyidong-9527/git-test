c0fc8d7c293424e462f09680ff6f1cbcf6d4df3c
liyidong <18339162892@163.com>
2022-06-09 16:58:42 +0800

    fix(aa)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { lazy, ReactNode, Suspense } from 'react';
 import './App.css';
-
+import Router from './router/Router';
+import { routers } from './router/config';
+import { Outlet } from 'react-router';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router routes={routers} />
+      <Outlet />
     </div>
   );
 }
