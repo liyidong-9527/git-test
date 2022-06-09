beeece1ee397667562ff221d29536623398ab4ed
liyidong <18339162892@163.com>
2022-06-09 16:48:12 +0800

    footer444

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -5,6 +5,7 @@ const Footer = () => (
   <div>
     这应该是尾部，但是我不知道写什么
     <p>ccc</p>
+    <p>4444</p>
     <Link to='/container'>aaa</Link>
     <Link to='/footer/a'>222</Link>
     <Link to='/case'>案件</Link>
